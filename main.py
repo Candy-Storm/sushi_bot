439d62655e2ca124667d995dea3c14eb7d0d4876
Candy-Storm <mishachaikawork@gmail.com>
2026-07-30 22:18:17 +0300

    123

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ kb_wheel = ReplyKeyboardMarkup(
     keyboard=[[
         KeyboardButton(
             text="🎰 Відкрити Рулетку", 
-            web_app=WebAppInfo(url="https://candy-storm.github.io/sushi_bot/index.html?v=16")
+            web_app=WebAppInfo(url="https://candy-storm.github.io/sushi_bot/index.html?v=17")
         )
     ]],
     resize_keyboard=True
